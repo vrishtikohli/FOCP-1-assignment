8610b469234961ff4f1da3b9f9cd4ab33cff0c53
vrishtikohli <vrishtikohli@gmail.com>
2025-11-02 13:52:29 +0530

    Implement peak element finder in C

---
+++ 13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    if (n == 1) {
+        printf("Peak element is %d (only element)\n", arr[0]);
+        return 0;
+    }
+    if (arr[0] >= arr[1]) {
+        printf("Peak element is %d (at index 0)\n", arr[0]);
+        return 0;
+    }
+    for (i = 1; i < n - 1; i++) {
+        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
+            printf("Peak element is %d (at index %d)\n", arr[i], i);
+            return 0;
+        }
+    }
+    if (arr[n - 1] >= arr[n - 2]) {
+        printf("Peak element is %d (at index %d)\n", arr[n - 1], n - 1);
+    }
+    return 0;
+}
